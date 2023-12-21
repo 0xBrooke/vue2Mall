dfcc461ac5cf95f4d2fbe5f85336be10b0e99f1c
Brooke-web3 <xiawenke@admindeMacBook-Pro.local>
2023-12-21 11:13:01 +0800

    test

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,11 @@
+const { publicDecrypt } = require("crypto");
 const path = require("path"); // 引入path模块
 function resolve(dir) {
   return path.join(__dirname, dir); //path.join(_dirname)设置绝对路径
 }
 
 module.exports = {
+  publicPath:'/vue2wMall/',
   chainWebpack: (config) => {
     //第一个参数：别名 第二个参数：路径
     config.resolve.alias
